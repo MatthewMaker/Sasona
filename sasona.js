0518221396327092aff1d3c6628eea490ba245eb
genieva <gtcroley@gmail.com>
2016-11-14 17:51:18 -0600

    Edit Sasona JS
    Still working on onclick()

--- sasona.js
+++ sasona.js
@@ -1,4 +1,4 @@
-document.getmyElementById("text").onclick= function(){
+document.getmyElementById("text").onclick = function () {
     document.getElementById("text").style.display = 'block';
     document.getElementById("text").style.visibility ='visible';
 };
