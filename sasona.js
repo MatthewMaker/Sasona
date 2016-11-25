78060303d8dba2e52c24341e58f291427ae07aeb
genieva <gtcroley@gmail.com>
2016-11-25 17:21:34 -0600

    Edit js
    working on making buttons work

--- sasona.js
+++ sasona.js
@@ -1,4 +1,12 @@
-document.getmyElementById("button").onclick = function () {
-    document.getElementById("text").style.display = 'block';
-    document.getElementById("text").style.visibility ='visible';
-};
+//document.getmyElementById("button").onclick = function () {
+//    document.getElementById("text").style.display = 'block';
+//    document.getElementById("text").style.visibility ='visible';
+//};
+function show(target){
+document.getElementById(target).style.display = 'block';
+document.getElementById("text").style.display = 'none';
+}
+function hide(target){
+document.getElementById(target).style.display = 'none';
+document.getElementById("text").style.display = 'block';
+}
