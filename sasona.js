fe360cecfeef582fa67016b2d2626fd769d13e6f
genieva <gtcroley@gmail.com>
2016-11-26 17:23:09 -0600

    Edit FAQ buttons
    click button to display text.

--- sasona.js
+++ sasona.js
@@ -3,10 +3,10 @@
 //    document.getElementById("text").style.visibility ='visible';
 //};
 function show(target){
-document.getElementById(target).style.display = 'block';
-document.getElementById("text").style.display = 'none';
+document.getElementById("button").onlclick.style.display = 'block';
+document.getElementById("text").onclick.style.display = 'block';
 }
-function hide(target){
-document.getElementById(target).style.display = 'none';
-document.getElementById("text").style.display = 'block';
-}
+//function hide(target){
+//document.getElementById("text").style.display = 'none';
+//document.getElementById("text").style.display = 'block';
+//}
