1b5db8e09a94c89bea620c01b25430692482a0f4
genieva <gtcroley@gmail.com>
2017-01-04 17:02:49 -0600

    Edit FAQ and js
    accordian FAQ

--- sasona.js
+++ sasona.js
@@ -2,11 +2,16 @@
 //    document.getElementById("text").style.display = 'block';
 //    document.getElementById("text").style.visibility ='visible';
 //};
-function show(target){
-document.getElementById("button").onlclick.style.display = 'block';
-document.getElementById("text").onclick.style.display = 'block';
-}
+
+//
+//function show(target){
+//document.getElementById("button").onlclick.style.display = 'block';
+//document.getElementById("text").onclick.style.display = 'block';
+//}
 //function hide(target){
 //document.getElementById("text").style.display = 'none';
 //document.getElementById("text").style.display = 'block';
 //}
+  $( function() {
+    $( "#answer" ).accordion();
+  } );
