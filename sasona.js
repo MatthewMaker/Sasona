f4bd04c5a06499909f7a1f3ae70f0fdd81ca0552
genieva <gtcroley@gmail.com>
2017-01-16 13:20:50 -0600

    Edit FAQ
    Edits to the JS to make the accordian function work. Show my answers.

--- sasona.js
+++ sasona.js
@@ -4,11 +4,11 @@ var i;
 for (i = 0; i < acc.length; i++) {
   acc[i].onclick = function() {
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-	  if (panel.style.maxHeight){
-  	  panel.style.maxHeight = null;
+    var answers = this.nextElementSibling;
+	  if (answers.style.maxHeight){
+  	  answers.style.maxHeight = null;
     } else {
-  	  panel.style.maxHeight = panel.scrollHeight + 'px';
+  	  answers.style.maxHeight = answers.scrollHeight + 'px';
     } 
   }
 }
