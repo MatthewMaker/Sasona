34309516cccdd272d8e26c20d74bb808592b4ec2
genieva <gtcroley@gmail.com>
2017-01-19 12:44:57 -0600

    Edit FAQ
    Fixed scope issue in accordion for FAQ

--- sasona.js
+++ sasona.js
@@ -2,7 +2,7 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].onclick = function() {
+  acc[i].onclick = function () {
     this.classList.toggle("active");
     var answers = this.nextElementSibling;
 	  if (answers.style.maxHeight){
