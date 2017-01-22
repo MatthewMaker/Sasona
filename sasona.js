ad106de9edd8dcc0b9b7c710e3854e455b5f16c1
genieva <gtcroley@gmail.com>
2017-01-22 13:47:53 -0600

    Edit CSS
    playing with buttons

--- sasona.js
+++ sasona.js
@@ -4,6 +4,7 @@ var i;
 for (i = 0; i < acc.length; i++) {
    acc[i].onclick = function () {
     this.classList.toggle("active");
+       
     var answers = this.nextElementSibling;
 	  if (answers.style.maxHeight){
   	  answers.style.maxHeight = null;
