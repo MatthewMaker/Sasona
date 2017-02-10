463dd5ef77fe25efcc1a5673b7090a5fba902bfa
genieva <gtcroley@gmail.com>
2017-02-10 13:14:07 -0600

    Edit About and js script
    slide show pls

--- sasona.js
+++ sasona.js
@@ -17,3 +17,26 @@ for (i = 0; i < acc.length; i++) {
 //$( function() {
 //    $( "#accordion" ).accordion();
 //  } ); playing with jquery but this didn't work like I thought it would   
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide_pic");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+	  slides[i].style.display = "none"; 
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  
+}
